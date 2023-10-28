1146763692fb8aff6e11d0afdbba946a52f8607d
Mahmoud.Eid <39015047+Mado007@users.noreply.github.com>
2023-10-28 17:42:37 +0200

    mysql dev

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,7 +1,10 @@
--- prepares a MySQL server for the project
-
+-- creates the database hbnb_dev_db in MySQL server
+-- create user hbnb_dev with pwd hbnb_dev_pwd
+-- grant hbnb_dev select on performance_schema
+-- grant all privileges on hbnb_dev_db
+-- only if database doesn't already exist
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
-GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
+GRANT ALL ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
